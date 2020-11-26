acd604356c162e015d81cb3ae39e80d73b51c13f
Ary S <arysugiarto10@gmail.com>
2020-11-26 21:36:34 +0700

    coroutin test

---
+++ kotlin-coroutine-unit-test/app/src/test/java/com/mindorks/example/coroutines/learn/retrofit/single/SingleNetworkCallViewModelTesst.kt
@@ -0,0 +1,11 @@
+package com.mindorks.example.coroutines.learn.retrofit.single
+
+import junit.framework.TestCase
+
+class SingleNetworkCallViewModelTesst : TestCase() {
+
+
+    fun testGetUsers() {
+
+    }
+}
